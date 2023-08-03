3605ed3f47ca9389b6039eb75940b52ecc187c42
Osvaldo Cuambe <95348230+OGCapsLock@users.noreply.github.com>
2023-08-03 18:11:15 +0000

    User created test User doesnt exist test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,24 @@
 import { describe, it } from "@jest/globals";
+import { newUser } from ".";
 
-
-describe("Say Hello",()=>{
-  it("Greats",()=>{
+describe("Say Hello", () => {
+  it("Greats", () => {
     expect("Hello world").toEqual("Hello world");
-  })
-})
+  });
+  it("Creates user", async () => {
+    const user = {
+      userName: "admin",
+      password: "1234",
+    };
+    const created = await newUser(user);
+    expect(created).toBeTruthy();
+  });
+  it("Fetch user fake user", async () => {
+    const user = {
+      userName: "admin1",
+      password: "1234",
+    };
+    const notFound = await fetchUser(user);
+    expect(notFound).toBeTruthy();
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import express, { Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
@@ -16,8 +16,26 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = Number(process.env.PORT) || 5000;
-app.listen(port, () => console.log("Servidor a correr na porta " + port));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+const prisma = new PrismaClient();
+
+export async function newUser(user:{userName:string,password:string}) {
+  return await prisma.user
+    .create({
+      data: {...user},
+    })
+    .then(() => {
+      return true;
+    })
+    .catch(() => {
+      return false;
+    });
+}
+
 const userRouter = express.Router();
+
+app.listen(port, () => console.log("Servidor a correr na porta " + port));
